dd890b90b4322924ccbe81f8a4a7006b4c2afc33
Alleycat404 <Rioland@126.com>
2023-03-11 17:47:05 +0800

    progress bar

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from Flickr import Flickr
 from pytorch_lightning.callbacks import RichProgressBar
 # from prog_bar import GlobalProgressBar
 
-root = "E:/datasets/flickr"
+root = "flickr"
 
 model = RNN_VAE(root)
 
@@ -17,10 +17,10 @@ train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True, num_worker
 valid_loader = DataLoader(valid_dataset, batch_size=2, shuffle=False, num_workers=0)
 
 checkpoint_callback = pl.callbacks.ModelCheckpoint(dirpath="ckpt", monitor="val_loss")
-callbacks = [RichProgressBar(leave=True), checkpoint_callback]
+callbacks = [checkpoint_callback]
 
 trainer = pl.Trainer(callbacks=callbacks, enable_progress_bar=True, fast_dev_run=False,
-                     max_epochs=10, accelerator='auto', check_val_every_n_epoch=2, auto_lr_find=True)
+                     max_epochs=100, accelerator='auto', check_val_every_n_epoch=2, auto_lr_find=True)
 
 trainer.fit(model)
 
