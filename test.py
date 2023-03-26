10e28b707e3e3a65bfe30425f60f1a3535eb0390
Alleycat404 <Rioland@126.com>
2023-03-26 09:15:04 +0800

    L==0.01

--- test.py
+++ test.py
@@ -1,12 +1,21 @@
 import pytorch_lightning as pl
 
 from models import RNN_VAE, RNN
+
+
 def inference():
     model = RNN_VAE('small_flickr')
     trainer = pl.Trainer(accelerator='auto')
     # trainer.fit(model)
     trainer.predict(model, ckpt_path='ckpt/R-D/L=0.5_epoch=27-step=40656.ckpt')
 
+def test():
+    model = RNN_VAE('flickr')
+    trainer = pl.Trainer(accelerator='auto')
+    trainer.test(model, ckpt_path='ckpt/epoch=45-step=66792.ckpt')
+
 
 if __name__ == '__main__':
-    inference()
+    test()
+
+
